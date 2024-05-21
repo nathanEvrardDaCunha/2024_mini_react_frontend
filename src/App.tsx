9179b34c46baae672b7eb63d1e4a8453909231b2
Zharks <nevraddacunha@myges.fr>
2024-05-21 12:23:10 +0200

    feat: delete an get all employee

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import {Navigate, Route, Routes} from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { BrowserRouter as Router } from 'react-router-dom';
 import Cookies from "js-cookie";
 import React from "react";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
+import EmployeePage from "./pages/EmployeePage";
 
-// @ts-ignore
-const PrivateRoute = ({ children }) => {
-  const authToken = Cookies.get("userId");
-  return authToken ? children : <Navigate to="/not-authorized" replace />;
+const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
+    const authToken = Cookies.get("userId");
+    return authToken ? <>{children}</> : <Navigate to="/" replace />;
 };
 
 const App = () => {
@@ -16,66 +16,18 @@ const App = () => {
         <Router>
             <Routes>
                 <Route path="/register" element={<RegisterPage />} />
-                <Route path="/login" element={<LoginPage />} />
+                <Route path="/" element={<LoginPage />} />
+                <Route
+                    path="/employee"
+                    element={
+                        <PrivateRoute>
+                            <EmployeePage />
+                        </PrivateRoute>
+                    }
+                />
             </Routes>
         </Router>
     )
 };
-
-/*
-function App() {
-  return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<RegisterPage />} />
-          <Route path="/register-location" element={<RegisterLocationPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/not-authorized" element={<NotAuthorizedPage />} />
-          <Route
-              path="/user/info"
-              element={
-                <PrivateRoute>
-                  <UserInfoPage />
-                </PrivateRoute>
-              }
-          />
-          <Route
-              path="/user/donation"
-              element={
-                <PrivateRoute>
-                  <UserDonationPage />
-                </PrivateRoute>
-              }
-          />
-          <Route
-              path="/user/membership"
-              element={
-                <PrivateRoute>
-                  <UserMembershipPage />
-                </PrivateRoute>
-              }
-          />
-          <Route
-              path="/user/location"
-              element={
-                <PrivateRoute>
-                  <UserLocationPage />
-                </PrivateRoute>
-              }
-          />
-          <Route
-              path="/user/create-location"
-              element={
-                <PrivateRoute>
-                  <CreateLocationPage />
-                </PrivateRoute>
-              }
-          />
-        </Routes>
-      </Router>
-  );
-}
-*/
-
 
 export default App;
---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,1 @@
+import React from 'react';interface ErrorMessageProps {	message: string;}const ErrorMessage: React.FC<ErrorMessageProps> = ({ message }) => {	return (		<div className="alert alert-danger text-center" role="alert">			{message}		</div>	);};export default ErrorMessage;
